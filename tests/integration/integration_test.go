c0a45ce8c0e5cd9605815750b53ad226f4af0d7c
Andriian Chestnykh <>
2023-11-20 15:01:31 +0200

    Add integration tests for ETH CLI

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -28,10 +28,9 @@ func TestProof_Http(t *testing.T) {
 }
 
 func TestProof_Eth(t *testing.T) {
-	t.Skip("skipping ETH CLI test")
-	addrStr, ok := os.LookupEnv("IDENTITY_TREE_STORE_ADDRESS")
+	addrStr, ok := os.LookupEnv("IRHS_STORAGE_ADDRESS")
 	if !ok {
-		panic("IDENTITY_TREE_STORE_ADDRESS not set")
+		panic("IRHS_STORAGE_ADDRESS not set")
 	}
 
 	addr := ethcommon.HexToAddress(addrStr)
