a48efbb1377eece0a94f8e07cea5c4ebea381f41
vbasiuk <volodymyr.basiuck@gmail.com>
2024-01-12 15:17:10 +0200

    fix OnChain Resolve

--- resolvers/onchain.go
+++ resolvers/onchain.go
@@ -22,7 +22,7 @@ type OnChainResolver struct {
 }
 
 // resolve is a method to resolve a credential status from the blockchain.
-func (*OnChainResolver) Resolve(status verifiable.CredentialStatus, cfg verifiable.CredentialStatusConfig) (out verifiable.RevocationStatus, err error) {
+func (OnChainResolver) Resolve(status verifiable.CredentialStatus, cfg verifiable.CredentialStatusConfig) (out verifiable.RevocationStatus, err error) {
 	parsedIssuerDID, err := w3c.ParseDID(*cfg.IssuerDID)
 	if err != nil {
 		return out, err
