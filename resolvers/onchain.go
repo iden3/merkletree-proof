69b8fc5ba5be112343b996be0422b007b7ddbaab
vbasiuk <volodymyr.basiuck@gmail.com>
2024-01-12 13:58:18 +0200

    add rhs/onchain resolvers

---
+++ resolvers/onchain.go
@@ -0,0 +1,245 @@
+package resolvers
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+
+	core "github.com/iden3/go-iden3-core/v2"
+	"github.com/iden3/go-iden3-core/v2/w3c"
+	"github.com/iden3/go-merkletree-sql/v2"
+	"github.com/iden3/go-schema-processor/v2/utils"
+	"github.com/iden3/go-schema-processor/v2/verifiable"
+	"github.com/pkg/errors"
+)
+
+// OnChainIssuer is a struct that allows to interact with the onchain contract and build the revocation status.
+type OnChainResolver struct {
+}
+
+// resolve is a method to resolve a credential status from the blockchain.
+func (*OnChainResolver) Resolve(status verifiable.CredentialStatus, cfg verifiable.CredentialStatusConfig) (out verifiable.RevocationStatus, err error) {
+	parsedIssuerDID, err := w3c.ParseDID(*cfg.IssuerDID)
+	if err != nil {
+		return out, err
+	}
+
+	issuerID, err := core.IDFromDID(*parsedIssuerDID)
+	if err != nil {
+		return out, err
+	}
+
+	var zeroID core.ID
+	if issuerID == zeroID {
+		return out, errors.New("issuer ID is empty")
+	}
+
+	onchainRevStatus, err := newOnchainRevStatusFromURI(status.ID)
+	if err != nil {
+		return out, err
+	}
+
+	if onchainRevStatus.revNonce != status.RevocationNonce {
+		return out, fmt.Errorf(
+			"revocationNonce is not equal to the one "+
+				"in OnChainCredentialStatus ID {%d} {%d}",
+			onchainRevStatus.revNonce, status.RevocationNonce)
+	}
+
+	isStateContractHasID, err := stateContractHasID(&issuerID, cfg.StateResolver)
+	if err != nil {
+		return out, err
+	}
+
+	var resp verifiable.RevocationStatus
+	if isStateContractHasID {
+		resp, err = cfg.StateResolver.GetRevocationStatus(issuerID.BigInt(),
+			onchainRevStatus.revNonce)
+		if err != nil {
+			msg := err.Error()
+			if isErrInvalidRootsLength(err) {
+				msg = "roots were not saved to identity tree store"
+			}
+			return out, fmt.Errorf(
+				"GetRevocationProof smart contract call [GetRevocationStatus]: %s",
+				msg)
+		}
+	} else {
+		if onchainRevStatus.genesisState == nil {
+			return out, errors.New(
+				"genesis state is not specified in OnChainCredentialStatus ID")
+		}
+		resp, err = cfg.StateResolver.GetRevocationStatusByIDAndState(
+			issuerID.BigInt(), onchainRevStatus.genesisState,
+			onchainRevStatus.revNonce)
+		if err != nil {
+			return out, fmt.Errorf(
+				"GetRevocationProof smart contract call [GetRevocationStatusByIdAndState]: %s",
+				err.Error())
+		}
+	}
+
+	return resp, nil
+}
+
+func newOnchainRevStatusFromURI(stateID string) (onChainRevStatus, error) {
+	var s onChainRevStatus
+
+	uri, err := url.Parse(stateID)
+	if err != nil {
+		return s, errors.New("OnChainCredentialStatus ID is not a valid URI")
+	}
+
+	contract := uri.Query().Get("contractAddress")
+	if contract == "" {
+		return s, errors.New("OnChainCredentialStatus contract address is empty")
+	}
+
+	contractParts := strings.Split(contract, ":")
+	if len(contractParts) != 2 {
+		return s, errors.New(
+			"OnChainCredentialStatus contract address is not valid")
+	}
+
+	s.chainID, err = newChainIDFromString(contractParts[0])
+	if err != nil {
+		return s, err
+	}
+	s.contractAddress = contractParts[1]
+
+	revocationNonce := uri.Query().Get("revocationNonce")
+	if revocationNonce == "" {
+		return s, errors.New("revocationNonce is empty in OnChainCredentialStatus ID")
+	}
+
+	s.revNonce, err = strconv.ParseUint(revocationNonce, 10, 64)
+	if err != nil {
+		return s, errors.New("revocationNonce is not a number in OnChainCredentialStatus ID")
+	}
+
+	// state may be nil if params is absent in query
+	s.genesisState, err = newIntFromHexQueryParam(uri, "state")
+	if err != nil {
+		return s, err
+	}
+
+	return s, nil
+}
+
+func newChainIDFromString(in string) (core.ChainID, error) {
+	var chainID uint64
+	var err error
+	if strings.HasPrefix(in, "0x") ||
+		strings.HasPrefix(in, "0X") {
+		chainID, err = strconv.ParseUint(in[2:], 16, 64)
+		if err != nil {
+			return 0, err
+		}
+	} else {
+		chainID, err = strconv.ParseUint(in, 10, 64)
+		if err != nil {
+			return 0, err
+		}
+	}
+	return core.ChainID(chainID), nil
+}
+
+// newIntFromHexQueryParam search for query param `paramName`, parse it
+// as hex string of LE bytes of *big.Int. Return nil if param is not found.
+func newIntFromHexQueryParam(uri *url.URL, paramName string) (*big.Int, error) {
+	stateParam := uri.Query().Get(paramName)
+	if stateParam == "" {
+		return nil, nil
+	}
+
+	stateParam = strings.TrimSuffix(stateParam, "0x")
+	stateBytes, err := hex.DecodeString(stateParam)
+	if err != nil {
+		return nil, err
+	}
+
+	return newIntFromBytesLE(stateBytes), nil
+}
+
+func newIntFromBytesLE(bs []byte) *big.Int {
+	return new(big.Int).SetBytes(utils.SwapEndianness(bs))
+}
+
+func stateContractHasID(id *core.ID, resolver verifiable.CredStatusStateResolver) (bool, error) {
+
+	idsInStateContractLock.RLock()
+	ok := idsInStateContract[*id]
+	idsInStateContractLock.RUnlock()
+	if ok {
+		return ok, nil
+	}
+
+	idsInStateContractLock.Lock()
+	defer idsInStateContractLock.Unlock()
+
+	ok = idsInStateContract[*id]
+	if ok {
+		return ok, nil
+	}
+
+	_, err := lastStateFromContract(resolver, id)
+	if errors.Is(err, errIdentityDoesNotExist) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	idsInStateContract[*id] = true
+	return true, err
+}
+
+type onChainRevStatus struct {
+	chainID         core.ChainID
+	contractAddress string
+	revNonce        uint64
+	genesisState    *big.Int
+}
+
+func isErrInvalidRootsLength(err error) bool {
+	if err == nil {
+		return false
+	}
+	return err.Error() == "execution reverted: Invalid roots length"
+}
+
+var errIdentityDoesNotExist = errors.New("identity does not exist")
+
+func isErrIdentityDoesNotExist(err error) bool {
+	if err == nil {
+		return false
+	}
+	return err.Error() == "execution reverted: Identity does not exist"
+}
+
+var idsInStateContract = map[core.ID]bool{}
+var idsInStateContractLock sync.RWMutex
+
+func lastStateFromContract(resolver verifiable.CredStatusStateResolver,
+	id *core.ID) (*merkletree.Hash, error) {
+	var zeroID core.ID
+	if id == nil || *id == zeroID {
+		return nil, errors.New("ID is empty")
+	}
+
+	resp, err := resolver.GetStateInfoByID(id.BigInt())
+	if isErrIdentityDoesNotExist(err) {
+		return nil, errIdentityDoesNotExist
+	} else if err != nil {
+		return nil, err
+	}
+
+	if resp.State == "" {
+		return nil, errors.New("got empty state")
+	}
+
+	return merkletree.NewHashFromString(resp.State)
+}
---
+++ resolvers/rhs.go
@@ -0,0 +1,179 @@
+package resolvers
+
+import (
+	"bytes"
+	"context"
+	"math/big"
+	"net/url"
+	"strings"
+	"time"
+
+	core "github.com/iden3/go-iden3-core/v2"
+	"github.com/iden3/go-iden3-core/v2/w3c"
+	"github.com/iden3/go-merkletree-sql/v2"
+	"github.com/iden3/go-schema-processor/v2/verifiable"
+	mp "github.com/iden3/merkletree-proof/http"
+	"github.com/pkg/errors"
+)
+
+// RHSResolver is a struct that allows to interact with the RHS service to get revocation status.
+type RHSResolver struct {
+}
+
+// resolve is a method to resolve a credential status from the RHS.
+func (RHSResolver) Resolve(status verifiable.CredentialStatus, cfg verifiable.CredentialStatusConfig) (out verifiable.RevocationStatus, err error) {
+	parsedIssuerDID, err := w3c.ParseDID(*cfg.IssuerDID)
+	if err != nil {
+		return out, err
+	}
+
+	issuerID, err := core.IDFromDID(*parsedIssuerDID)
+	if err != nil {
+		return out, err
+	}
+
+	revNonce := new(big.Int).SetUint64(status.RevocationNonce)
+
+	baseRHSURL, genesisState, err := rhsBaseURL(status.ID)
+	if err != nil {
+		return out, err
+	}
+
+	state, err := identityStateForRHS(cfg.StateResolver, &issuerID, genesisState)
+	if err != nil {
+		return out, err
+	}
+
+	rhsCli, err := newRhsCli(baseRHSURL)
+	if err != nil {
+		return out, err
+	}
+
+	out.Issuer, err = issuerFromRHS(context.Background(), *rhsCli, state)
+	if errors.Is(err, mp.ErrNodeNotFound) {
+		if genesisState != nil && state.Equals(genesisState) {
+			return out, errors.New("genesis state is not found in RHS")
+		} else {
+			return out, errors.New("current state is not found in RHS")
+		}
+	} else if err != nil {
+		return out, err
+	}
+
+	revNonceHash, err := merkletree.NewHashFromBigInt(revNonce)
+	if err != nil {
+		return out, err
+	}
+
+	revTreeRootHash, err := merkletree.NewHashFromHex(*out.Issuer.RevocationTreeRoot)
+	if err != nil {
+		return out, err
+	}
+	proof, err := rhsCli.GenerateProof(context.Background(), revTreeRootHash,
+		revNonceHash)
+	if err != nil {
+		return out, err
+	}
+
+	out.MTP = *proof
+
+	return out, nil
+}
+
+func identityStateForRHS(resolver verifiable.CredStatusStateResolver, issuerID *core.ID,
+	genesisState *merkletree.Hash) (*merkletree.Hash, error) {
+
+	state, err := lastStateFromContract(resolver, issuerID)
+	if !errors.Is(err, errIdentityDoesNotExist) {
+		return state, err
+	}
+
+	if genesisState == nil {
+		return nil, errors.New("current state is not found for the identity")
+	}
+
+	stateIsGenesis, err := genesisStateMatch(genesisState, *issuerID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !stateIsGenesis {
+		return nil, errors.New("state is not genesis for the identity")
+	}
+
+	return genesisState, nil
+}
+
+// check if genesis state matches the state from the ID
+func genesisStateMatch(state *merkletree.Hash, id core.ID) (bool, error) {
+	var tp [2]byte
+	copy(tp[:], id[:2])
+	otherID, err := core.NewIDFromIdenState(tp, state.BigInt())
+	if err != nil {
+		return false, err
+	}
+	return bytes.Equal(otherID[:], id[:]), nil
+}
+
+func issuerFromRHS(ctx context.Context, rhsCli mp.ReverseHashCli,
+	state *merkletree.Hash) (verifiable.Issuer, error) {
+
+	var issuer verifiable.Issuer
+
+	stateNode, err := rhsCli.GetNode(ctx, state)
+	if err != nil {
+		return issuer, err
+	}
+
+	if len(stateNode.Children) != 3 {
+		return issuer, errors.New(
+			"invalid state node, should have 3 children")
+	}
+
+	stateHex := state.Hex()
+	issuer.State = &stateHex
+	claimsTreeRootHex := stateNode.Children[0].Hex()
+	issuer.ClaimsTreeRoot = &claimsTreeRootHex
+	revocationTreeRootHex := stateNode.Children[1].Hex()
+	issuer.RevocationTreeRoot = &revocationTreeRootHex
+	rootOfRootsHex := stateNode.Children[2].Hex()
+	issuer.RootOfRoots = &rootOfRootsHex
+
+	return issuer, err
+}
+
+func newRhsCli(rhsURL string) (*mp.ReverseHashCli, error) {
+	if rhsURL == "" {
+		return nil, errors.New("reverse hash service url is empty")
+	}
+
+	return &mp.ReverseHashCli{
+		URL:         rhsURL,
+		HTTPTimeout: 10 * time.Second,
+	}, nil
+}
+
+func rhsBaseURL(rhsURL string) (string, *merkletree.Hash, error) {
+	u, err := url.Parse(rhsURL)
+	if err != nil {
+		return "", nil, err
+	}
+	var state *merkletree.Hash
+	stateStr := u.Query().Get("state")
+	if stateStr != "" {
+		state, err = merkletree.NewHashFromHex(stateStr)
+		if err != nil {
+			return "", nil, err
+		}
+	}
+
+	if strings.HasSuffix(u.Path, "/node") {
+		u.Path = strings.TrimSuffix(u.Path, "node")
+	}
+	if strings.HasSuffix(u.Path, "/node/") {
+		u.Path = strings.TrimSuffix(u.Path, "node/")
+	}
+
+	u.RawQuery = ""
+	return u.String(), state, nil
+}
